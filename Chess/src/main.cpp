067979c0a615cfe479e45b5376382581333533c0
Holger Windahl <holger.windahl@gmail.com>
2022-08-17 11:18:32 +0200

    asdd

--- Chess/src/main.cpp
+++ Chess/src/main.cpp
@@ -1,11 +1,31 @@
-#include <iostream>
-
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
+#include <SFML\Graphics.hpp>
+#include <cassert>
 
 int main() {
-	std::cout << "Hello";
+    sf::RenderWindow sfmlWin(sf::VideoMode(600, 360), "Hello World SFML Window");
+    sf::Font font;
+    //You need to pass the font file location
+    if (!font.loadFromFile(/*
+                           Put the filename that identify the font file you want to load*/"myfont.ttf")) {
+        return -1;
+    }
+    sf::Text message("Hello, World !", font);
+
+    while (sfmlWin.isOpen()) {
+
+        sf::Event e;
+        while (sfmlWin.pollEvent(e)) {
+
+            switch (e.type) {
+            case sf::Event::EventType::Closed:
+                sfmlWin.close();
+                break;
+            }
+        }
+
+        sfmlWin.clear();
+        sfmlWin.draw(message);
+        sfmlWin.display();
+    }
+    return 0;
 }
